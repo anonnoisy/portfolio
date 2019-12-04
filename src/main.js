834e5437a60d8109b401632fec7cc6d2e6296701
anonnoisy <batuhc105@gmail.com>
2019-12-05 01:02:31 +0700

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import CarbonComponentsVue from '@carbon/vue/src/index'
 
 Vue.config.productionTip = false
+Vue.use(CarbonComponentsVue)
 
 new Vue({
   render: h => h(App),
